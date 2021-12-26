70b4676650dc2cca6219bf534427823e2cffabac
dcl666 <71249820+dcl666@users.noreply.github.com>
2021-12-27 05:13:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import first_OOD.first_ood as fs
 import second_OOD.GLCM_SVM as GS
 import second_OOD.Mahalanobis_distance as Ma
-
+import classification_model.DenseNet169 as Classify
 if __name__ == '__main__':
     fs.first_OOD()
     GS.detect()
     Ma.detect()
+    Classify.final_classify()
