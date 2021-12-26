362b673ef9cae01a4c7f3573a91ec80a4a96c4b2
dcl666 <71249820+dcl666@users.noreply.github.com>
2021-12-27 05:11:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import first_OOD.first_ood as SVM
-import GLCM_SVM as GS
-import Mahalanobis_distance as Ma
+import first_OOD.first_ood as fs
+import second_OOD.GLCM_SVM as GS
+import second_OOD.Mahalanobis_distance as Ma
 
 if __name__ == '__main__':
-  
+    fs.first_OOD()
     GS.detect()
     Ma.detect()
